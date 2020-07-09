16dc2b891bf4391f7127e685f11f4204c8aacac5
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2020-07-09 16:07:50 +0300

    Add App Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
